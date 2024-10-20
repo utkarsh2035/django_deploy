aecb730aa48334bcefb0ce4f84e0dfba0aee3df5
utkarsh2035 <utkarshrawat2035@gmail.com>
2024-10-20 18:47:54 +0530

    logRequest middleware in setting

--- django_app/settings.py
+++ django_app/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_app.middleware.LogRequestMiddleware',  # Add custom middleware
+    # 'django_app.middleware.LogRequestMiddleware',  # Add custom middleware
     'django_app.middleware.CustomHeaderMiddleware',  # Add custom middleware
 ]
 
